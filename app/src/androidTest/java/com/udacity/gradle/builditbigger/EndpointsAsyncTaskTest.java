afd8046414f82093792c83e0626bdd92d44683e5
zam <ajmzamora@gmail.com>
2020-06-28 04:45:45 -0400

    remove ads and unnecessary dependencies
    fix test cases and adjust layout

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -16,6 +17,12 @@ import static org.junit.Assert.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
+    private static final String APP_PACKAGE_NAME = "com.udacity.builditbigger";
+    private static final String FREE_PACKAGE_NAME = APP_PACKAGE_NAME.concat(".flavors.free.free");
+    private static final String PAID_PACKAGE_NAME = APP_PACKAGE_NAME.concat(".flavors.paid.paid");
+    private static final String FREE_FLAVOR = "free";
+    private static final String PAID_FLAVOR = "paid";
+
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<MainActivity>(MainActivity.class);
@@ -24,7 +31,11 @@ public class EndpointsAsyncTaskTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.udacity.gradle.builditbigger", appContext.getPackageName());
+        Log.v("PKG" , appContext.getOpPackageName());
+        if(BuildConfig.FLAVOR.equals(PAID_FLAVOR))
+            assertEquals(PAID_PACKAGE_NAME, appContext.getPackageName());
+        else if(BuildConfig.FLAVOR.equals(FREE_FLAVOR))
+            assertEquals(FREE_PACKAGE_NAME, appContext.getPackageName());
     }
 
     @Test
